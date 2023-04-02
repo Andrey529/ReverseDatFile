4b97060b1487474a1534ca1ceb89cf7d6808e244
Andrey <75383288+Andrey529@users.noreply.github.com>
2023-04-03 00:27:48 +0300

    added time measurements and removed unnecessary code

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <vector>
+#include <chrono>
 
-//#include <random>
-//#include <chrono>
+// Link to github https://github.com/Andrey529/ReverseDatFile
 
 int main(int argc, char **argv) {
     if (argc != 3) {
         std::cerr << "Wrong number of arguments. Must be: "
                      "<program name> <input_file.dat> <output_file.dat>";
+        return -1;
     }
 
     try {
+        auto start = std::chrono::high_resolution_clock::now();
+
         std::ifstream inputFile(argv[1], std::ios::in | std::ios::binary);
 
         // Get the file size
@@ -41,48 +43,12 @@ int main(int argc, char **argv) {
             return -1;
         }
 
-
-
-//        std::fstream outputFile(argv[1], std::ios::out | std::ios::binary);
-//
-//        std::mt19937 generator(std::chrono::steady_clock::now().time_since_epoch().count()); //generator random values
-//        std::uniform_int_distribution<> range(0, 255);
-//
-//        std::ofstream testFile("D:\\Projects\\Clion\\ReverseDatFile\\data\\test.txt", std::ios::out);
-//
-//        for (int i = 0; i < 10'000'000; ++i) {
-//            char c = range(generator);
-//            outputFile.write(&c, sizeof(char));
-//            testFile << static_cast<int>(c) << std::endl;
-//        }
-//        outputFile.close();
-//        testFile.close();
-
-
-
-//        std::ifstream inputFile(argv[2], std::ios::in | std::ios::binary);
-//
-//        // Get the file size
-//        inputFile.seekg(0, std::ios::end);
-//        std::streamsize size = inputFile.tellg();
-//        inputFile.seekg(0, std::ios::beg);
-//
-//        std::vector<char> buffer(size);
-//
-//        // Reading data from a file
-//        if (inputFile.is_open()) {
-//            inputFile.read(buffer.data(), size);
-//            inputFile.close();
-//        } else {
-//            std::cerr << "Failed to open file for reading data." << std::endl;
-//        }
-//        std::ofstream testFileReverse("D:\\Projects\\Clion\\ReverseDatFile\\data\\testFileReverse.txt", std::ios::out);
-//        for (auto item : buffer) {
-//            testFileReverse << static_cast<int>(item) << std::endl;
-//        }
-
+        auto finish = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - start);
+        std::cout << "Execution time = " << finish.count() << " nanosec = " << finish.count()/1000 << " microsec = "
+                  << finish.count()/1000000 << " ms = " << finish.count()/1000000000 << " sec." << std::endl;
     } catch (const std::exception &ex) {
         std::cerr << ex.what() << std::endl;
+        return -1;
     }
     return 0;
 }
